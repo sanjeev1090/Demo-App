dde66fbbce04d4e13d8b713284c867a27971a59f
sanjeev.mishra <sanjeev.mishra@timesinternet.in>
2020-03-11 14:03:09 +0530

    demo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
+import {BrowserRouter,Route,Link} from 'react-router-dom'
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Basic from '../src/form/detail-form';
+import Introduction from '../src/intro/intro'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Hello World</div>
       </header>
+      <BrowserRouter>
+        <Route path="/basic" component={Basic} />
+        <Route path="/basic2" component={Introduction} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/intro/intro.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Introduction = () => (
+    <div>
+        This is second rout
+    </div>
+)
+export default Introduction;
